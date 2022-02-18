0e0454fc856a66f7d383e35f234541237a5ac965
Brad Buran <info@bradburan.com>
2022-02-18 11:39:33 -0600

    Add utility function to decode trigger bits

--- pyactivetwo/client.py
+++ pyactivetwo/client.py
@@ -14,6 +14,59 @@ import socket
 import numpy as np
 
 
+SPEED_MODE = {
+    0: 2048,
+    1: 4096,
+    2: 8192,
+    3: 16384,
+    4: 2048,
+    5: 4096,
+    6: 8192,
+    7: 16384,
+}
+
+
+def is_set(x, bit):
+    # Parentheses are not needed because of operator precedence rules, but help
+    # make the code more readable.
+    return (x & (1 << bit)) != 0
+
+
+def decode_trigger(x):
+    '''
+    Details
+    -------
+    Bit 00 (LSB) through 15:
+        Trigger input 1 through 16. Note that function keys F1 through F6 can embed
+        trigger bits in bits 8-15 if desired.
+    Bit 16 High when new Epoch is started
+    Bit 17 Speed bit 0
+    Bit 18 Speed bit 1
+    Bit 19 Speed bit 2
+    Bit 20 High when CMS is within range
+    Bit 21 Speed bit 3
+    Bit 22 High when battery is low
+    Bit 23 (MSB) High if ActiveTwo MK2
+    '''
+    speed_mode = \
+        (int(is_set(x, 17)) << 0) + \
+        (int(is_set(x, 18)) << 1) + \
+        (int(is_set(x, 19)) << 2) + \
+        (int(is_set(x, 21)) << 3)
+
+    trigger = 0b1111111111111111
+
+    return {
+        'trigger': x & trigger,
+        'cms_in_range': is_set(x, 20),
+        'low_battery': is_set(x, 22),
+        'ActiveMK2': is_set(x, 23),
+        'speed_mode': speed_mode,
+        'new_epoch': is_set(x, 16),
+        'fs': SPEED_MODE[speed_mode],
+    }
+
+
 class ActiveTwoClient:
     """
     Client for communicating with Biosemi ActiveTwo
